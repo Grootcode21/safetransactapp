86a13951e65206d701959696987842e1c2909391
Grootcode21 <antonyndichu15@gmail.com>
2021-12-06 01:16:25 -0800

    My first commit with templates and model

---
+++ accountlist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountlistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accountlist'
---
+++ accountlist/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Accountlist(models.Model):
+    text = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.text
