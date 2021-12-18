273b04049c068c1d43cf2d3a856decc77c37ab4f
Grootcode21 <antonyndichu15@gmail.com>
2021-12-17 22:52:54 -0800

    Updated the index page

--- accountlist/forms.py
+++ accountlist/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class AccountListForm(forms.Form):
     text = forms.CharField(max_length = 45,
     widget = forms.TextInput(
-        attrs = {'class':'form-control','placeholder':'Enter here','aria-label':'Account','aria-describeby':'add-btn'}
+        attrs = {'class':'form-control','placeholder':'Type here','aria-label':'Account','aria-describeby':'add-btn'}
     ))
